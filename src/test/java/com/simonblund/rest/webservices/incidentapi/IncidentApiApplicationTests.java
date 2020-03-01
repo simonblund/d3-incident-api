3d8f4de9994a5a3a97cfdf166d10ea31c084a9da
Simon Blomsterlund <simon@simonblund.com>
2020-03-01 12:36:46 +0200

    First commit

---
+++ src/test/java/com/simonblund/rest/webservices/incidentapi/IncidentApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.simonblund.rest.webservices.incidentapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IncidentApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
